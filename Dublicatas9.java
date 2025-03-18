fd4d7adc11f2912469012e21971900576e2cb350
Felipe B. Dutra <felipebardutra516@gmail.com>
2025-03-17 21:24:35 -0300

    17/3/25

--- Dublicatas9.java
+++ Dublicatas9.java
@@ -1,9 +1,36 @@
 package EX8;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+
 /**
  *
  * @author ADM
  */
 public class Dublicatas9 {
+    
+ public static void main(String[] args) {
 
+        Scanner teclado = new Scanner(System.in);
+
+        ArrayList<Integer> listaDeNomes = new ArrayList<>();
+
+        System.out.println("Digite um numero");
+        int n1 = teclado.nextInt();
+
+        System.out.println("Digite um numero");
+        int n2 = teclado.nextInt();
+
+     listaDeNomes.add(n1);
+        listaDeNomes.add(n2);
+    
+    HashSet<Integer> set = new HashSet<>(listaDeNomes);
+boolean hashDuplicatas = set.size() != listaDeNomes.size();   
+        
+    System.out.println("Os numeros sao iguais"+ hashDuplicatas);
+        
+        
+   
+ }
 }
---
+++ Remover11.java
@@ -0,0 +1,38 @@
+package EX8;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+/**
+ *
+ * @author ADM
+ */
+public class Remover11 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> listaDeNomes = new ArrayList<>();
+
+        listaDeNomes.add(1);
+        listaDeNomes.add(2);
+        listaDeNomes.add(2);
+        listaDeNomes.add(4);
+        listaDeNomes.add(5);
+        listaDeNomes.add(6);
+        listaDeNomes.add(7);
+        listaDeNomes.add(7);
+        listaDeNomes.add(9);
+        listaDeNomes.add(10);
+
+        
+        
+         
+        if (listaDeNomes != listaDeNomes) {
+
+
+             System.out.println(listaDeNomes);
+            
+        }
+
+    }
+}
